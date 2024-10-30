efe6cb11ac1971833c141d42b231abc8fe967c5f
William Shepherd <will.shepherdy.2002@gmail.com>
2024-10-30 20:28:18 +0000

    gay

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,16 +2,16 @@ import React from 'react';
 import '../styles/Navigation.css';
 
 const NavBar = () => {
-    const currentPath = window.location.pathname;
+    const currentPath = window.location.hash.replace('#', '');
     console.log('Current Path:', currentPath);
 
     return (
         <div className="topnav" id="myTopnav">
             <div className="title">William Shepherd Photography (攝影)</div>
             <div className="nav-links">
-                <a href="/" className={`nav-link ${currentPath === '/' ? 'active-link' : ''}`}>Portfolio</a>
-                <a href="/about" className={`nav-link ${currentPath === '/about' ? 'active-link' : ''}`}>About</a>
-                <a href="/contact" className={`nav-link ${currentPath === '/contact' ? 'active-link' : ''}`}>Contact</a>
+                <a href="#/" className={`nav-link ${currentPath === '/' ? 'active-link' : ''}`}>Portfolio</a>
+                <a href="#/about" className={`nav-link ${currentPath === '/about' ? 'active-link' : ''}`}>About</a>
+                <a href="#/contact" className={`nav-link ${currentPath === '/contact' ? 'active-link' : ''}`}>Contact</a>
             </div>
         </div>
     );
